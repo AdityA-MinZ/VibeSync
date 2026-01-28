906bcff883a9549c10b57a45703d8b979e31d0c2
AdiTyA MinZ <adityaminz@gmail.com>
2026-01-28 12:20:41 +0530

    fix render port binding 0.0.0.0

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,8 @@ const io = new Server(server, {
 // Middlewares
 app.use(cors());
 app.use(express.json());
+app.use('/api/friends', require('./routes/friends'));
+
 
 // MongoDB
 mongoose.connect(process.env.MONGODB_URI)
